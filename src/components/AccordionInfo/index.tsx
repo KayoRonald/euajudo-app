5a7788e06b1fb8319ee78a13f844b92f2d06f748
Kayo Ronald <kayoronald4321@gmail.com>
2021-06-07 22:27:07 +0000

    Adding type

--- src/components/AccordionInfo/index.tsx
+++ src/components/AccordionInfo/index.tsx
@@ -1,7 +1,61 @@
-import React from 'react'
+import React from 'react';
+import {
+  Accordion as ChakraAccordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
+  Box, Container, Heading, Center,
+} from '@chakra-ui/react'
+import faqDate from '../../json/faq/faq.json'
+import { Circle, StrongText, SpanText } from './style'
 
-export default function Accordion(){
-  return(
-    <h1>Hello, World</h1>
+interface AccordionProps {
+  id?: number;
+  header?: string;
+  body?: string;
+}
+
+const Accordion: React.FC<AccordionProps> = ({ id, header, body }) => {
+  return (
+    <Container maxWidth="74ch" mt="22px">
+      <Center marginBottom={5}>
+        <Heading as="h2">Perguntas frequentes</Heading>
+      </Center>
+      <ChakraAccordion allowMultiple>
+        {faqDate.map((faq) => (
+          <AccordionItem key={faq.id} borderTopWidth={0} borderBottomWidth={0} marginBottom="3px">
+            <h2>
+              <AccordionButton
+                padding="0.8em 1.2em 0.8em 1.2em"
+                background="gray.700"
+                _hover={{ backgroundColor: 'gray.600' }}
+              >
+                <Box flex="1" textAlign="left">
+                  <StrongText>
+                    <SpanText >
+                      0
+                      <span style={{ color: '#04D361' }}>
+                        {faq.id}
+                      .
+                    </span>
+                    </SpanText>
+                  </StrongText>
+                  {' '}
+                  {faq.header}
+                </Box>
+                <Circle>
+                  <AccordionIcon />
+                </Circle>
+              </AccordionButton>
+            </h2>
+            <AccordionPanel pb={4} background="#2d2d2d">
+              {faq.body}
+            </AccordionPanel>
+          </AccordionItem>
+        ))}
+      </ChakraAccordion>
+    </Container>
   )
 }
+export default Accordion
