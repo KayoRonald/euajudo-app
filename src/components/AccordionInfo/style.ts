9da55329738fefa685b5e4268887b10ffb06433e
Kayo Ronald <kayoronald4321@gmail.com>
2021-06-07 22:27:18 +0000

    create style

---
+++ src/components/AccordionInfo/style.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Circle = styled.div`
+   width: 30px;
+   height: 30px;
+   border-radius: 15px;
+   border: 2px solid #04D361;
+`;
+
+export const StrongText = styled.strong`
+  font-size: 16px;
+`
+export const SpanText = styled.span`
+  color: '#737380';
+`
