4c7ff548692c8942eefe0b5586844aea2a990914
Kayo Ronald <kayoronald4321@gmail.com>
2021-06-12 10:44:05 -0300

    removing comments

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -3,7 +3,3 @@ export { default as About } from './About/About';
 export { default as PageNotFound } from './PageNotFound/PageNotFound';
 export { default as Map } from './Map/Map';
 export { default as Team } from './Team/Team';
-// export { default as Login } from './Login/Login';
-// export { default as Signup } from './Signup/Signup';
-// export { default as PageNotFound } from './PageNotFound/PageNotFound';
-// export { default as ForgotPassword } from './ForgotPassword/ForgotPassword';
