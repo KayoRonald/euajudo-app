3cab2a1c68997a77054056973bd44b895c8b9a01
Kayo Ronald <kayoronald4321@gmail.com>
2021-07-10 10:46:43 +0000

    Create component Header

---
+++ src/components/FormHeader/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Box, Heading, Text } from '@chakra-ui/react';
+
+type FormPorps ={
+  heading: string;
+  text: string;
+}
+
+const FormHeader: React.FC<FormPorps> = ({heading, text}) => {
+  return (
+    <Box textAlign="center">
+      <Heading mb={2}>{heading}</Heading>
+      <Text>{text}</Text>
+    </Box>
+  )
+}
+export default FormHeader;
--- src/pages/CreatePoint/CreatePoint.tsx
+++ src/pages/CreatePoint/CreatePoint.tsx
@@ -1,21 +1,44 @@
-import React, { Fragment} from 'react'
+import React, { Fragment } from 'react';
 import Helmet from 'react-helmet';
-// import {
-//   Flex,
-//   Box, Heading, Text,
-//   FormControl, FormLabel,
-//   Textarea, Button
-// } from '@chakra-ui/react';
+import {
+  Flex,Box
+} from '@chakra-ui/react';
+import ContactForm from './ContactForm/'
+import FormHeader from '../../components/FormHeader/'
 
-const CreatePoint: React.FC = () => {
+const CreatePont: React.FC = () => {
   return (
     <Fragment>
-      <Helmet>
-        <title>Eu Ajudo | Cadastramentos de pontos</title>
-      </Helmet>
-      <h1>Create point</h1> 
+      <Helmet title="Eu Ajudo | Criação de Pontos" />
+      <Flex
+        id="anime"
+        minHeight="100vh"
+        width="full"
+        align="center"
+        justifyContent="center"
+      >
+        <Box
+          as="form"
+          backgroundColor="gray.700"
+          px={4}
+          width="94%"
+          maxWidth="700px"
+          mt={6}
+          borderRadius="sm"
+          textAlign="center"
+          mb={6}
+        >
+          <Box p={4}>
+            <FormHeader 
+              heading="Criação de Pontos" 
+              text="Vem ajudar sua comunidade!"
+            />
+            <ContactForm />
+          </Box>
+        </Box>
+      </Flex>
     </Fragment>
   )
 }
 
-export default CreatePoint;
+export default CreatePont
