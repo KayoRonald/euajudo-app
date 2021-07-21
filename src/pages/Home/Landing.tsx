652addc42a594fdeb310ac5236fe489f85cb5141
Kayo Ronald <kayoronald4321@gmail.com>
2021-07-21 18:10:43 +0000

    Update title

--- src/pages/Home/Landing.tsx
+++ src/pages/Home/Landing.tsx
@@ -1,16 +1,17 @@
 import React, { Fragment } from 'react';
 import Container from '../../containers/Jumbotron/';
 import Hero from '../../containers/Hero/'
-import Accordion  from '../../components/AccordionInfo/'
+import Accordion from '../../components/AccordionInfo/'
 import FooterContainer from '../../containers/Footer/'
-
+import Helmet from 'react-helmet';
 export default function Landing() {
   return (
     <Fragment>
+      <Helmet title="Eu Ajudo | Venha ajudar sua comunidade!" />
       <Hero />
       <Container />
       <Accordion />
-      <FooterContainer/>
+      <FooterContainer />
     </Fragment>
   );
 }
