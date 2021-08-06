d813e8b90246274300622cf837d58b9bbdd1bf76
Kayo Ronald <kayoronald4321@gmail.com>
2021-08-06 01:11:52 +0000

    Adding goBack

--- src/pages/PageNotFound/PageNotFound.tsx
+++ src/pages/PageNotFound/PageNotFound.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import Helmet from 'react-helmet';
 import Lottie from 'react-lottie';
+import { useHistory } from 'react-router-dom';
 import { AiOutlineArrowRight } from 'react-icons/ai';
 import { Stack, Button, Center, Text } from '@chakra-ui/react';
 import { Container } from './style';
 import animationData2 from '../../json/animation/62541-404-error-robot.json';
-import * as ROUTES from '../../constants/routes';
 import FooterContainer from '../../containers/Footer/';
 
-interface LocationProps {
+type LocationProps = {
   location?: any;
   pathname?: any;
 }
@@ -19,7 +19,7 @@ const PageNotFound: React.FC<LocationProps> = ({ location, pathname }) => {
     autoplay: true,
     animationData: animationData2,
   };
-
+  const { goBack } = useHistory();
   return (
     <React.Fragment key="key">
       <Container>
@@ -36,8 +36,8 @@ const PageNotFound: React.FC<LocationProps> = ({ location, pathname }) => {
         </Center>
 
         <Stack direction="row" spacing={4} align="center" mt={3}>
-            <Button as="a" href={ROUTES.HOME} colorScheme="teal" variant="solid" color="#000" leftIcon={<AiOutlineArrowRight />}>
-              Voltar para Home
+            <Button as="a" onClick={goBack} cursor="pointer" colorScheme="teal" variant="solid" color="#000" leftIcon={<AiOutlineArrowRight />}>
+              Voltar
             </Button>
         </Stack>
       </Container>
