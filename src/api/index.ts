fcdb4e507c41ac29d9806f6f1d1b82d80a4c4a1b
Kayo Ronald <kayoronald4321@gmail.com>
2021-08-08 06:06:09 -0300

    Update

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://euajudo-back-end.herokuapp.com',
+  baseURL: 'https://euajudo-back-end.herokuapp.com/',
 });
 
 export default api;
