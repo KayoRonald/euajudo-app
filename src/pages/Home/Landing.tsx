49865d4127d13fb47c0cb1cc6b66061e973bc8ec
Kayo Ronald <kayoronald4321@gmail.com>
2021-09-28 10:32:38 -0300

    Start of HubSpot Embed Code

--- src/pages/Home/Landing.tsx
+++ src/pages/Home/Landing.tsx
@@ -109,6 +109,7 @@ export default function Landing() {
         <Banner />
       </Box>
       <Footer />
+      <script type="text/javascript" id="hs-script-loader" async defer src="//js-na1.hs-scripts.com/20784564.js"></script>
     </React.Fragment>
   );
 }
